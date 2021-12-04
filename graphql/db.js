39b984446062212b901820b3bfa4249cacc89a41
Son Beomsoo <k1a11220@naver.com>
2021-12-04 23:28:53 +0900

    Implement category filter with two ways

--- graphql/db.js
+++ graphql/db.js
@@ -193,21 +193,21 @@ const benefitList = [
     period: "", //행사기간
   },
   {
-    id: 10, // 서비스 아이디
-    name: "", //업체 이름
-    category: "", //카테고리
-    location: "", //지역
-    address: "", //주소
-    summary: "", //상세페이지 소개 멘트
-    thumbnail: "", //카드 썸네일
-    mainBenefit: "", //혜택 요약
-    benefits: [""], //상세 혜택
-    conditions: [""], //혜택 조건
-    otherOpts: [""], //기타 주의사항
-    dtlImgs: [""], //상세페이지 이미지
-    dtlLink: "", //혜택 상세 링크 (이메일 포함)
-    contactNb: "", //연락처
-    period: "", //행사기간
+    id: 10,
+    name: "",
+    category: "",
+    location: "",
+    address: "",
+    summary: "",
+    thumbnail: "",
+    mainBenefit: "",
+    benefits: [""],
+    conditions: [""],
+    otherOpts: [""],
+    dtlImgs: [""],
+    dtlLink: "",
+    contactNb: "",
+    period: "",
   },
 ];
 export const getBenefitList = () => benefitList;
@@ -221,5 +221,5 @@ export const getByCategory = (category) => {
   const filteredBenefits = benefitList.filter(
     (benefit) => benefit.category === category
   );
-  return filteredBenefits[0];
+  return filteredBenefits;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,7 +2,10 @@ import { getBenefitList, getByCategory, getById } from "./db.js";
 
 const resolvers = {
   Query: {
-    benefits: () => getBenefitList(),
+    benefits(args) {
+      const { category } = args;
+      return getBenefitList().filter((a) => a.category == category);
+    },
     getById: (_, { id }) => getById(id),
     getByCategory: (_, { category }) => getByCategory(category),
   },
